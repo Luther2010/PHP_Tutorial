a388e0cda32a060cf373d72c826de6ac2ad3b9ed
Lu <Luther2020@gmail.com>
2014-03-26 11:56:49 -0700

    first.php added

---
+++ first.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+
+<h1>My first PHP page</h1>
+
+<?php
+echo "Hello World";
+$color = "red";
+echo $color;
+// Comment 1
+# Comment 2
+/* 
+Multiline Comment
+lalala
+*/
+?>
+
+</body>
+</html>
